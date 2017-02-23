c590229ad81962b653bffd1cd1e546795c62dd80
liuyun <liuyun@ihangmei.com>
2017-02-23 17:35:03 +0800

    服务

---
+++ 5.services/decorator.js
@@ -0,0 +1,16 @@
+function coffee(){
+    console.log('coffee');
+}
+
+function sweetCoff(){
+    coffee();
+    console.log('add sugar');
+}
+
+
+function saltCoff(){
+    sweetCoff
+    console.log('add salt');
+}
+
+saltCoff();
